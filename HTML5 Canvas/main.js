ae64996cb7568eef0b316be54f724c26fc89f003
Jesse Ma <j.masonoma17@gmail.com>
2018-06-11 15:02:10 -0700

    Add eventlisteners for mouse options and canvas functions

--- HTML5 Canvas/main.js
+++ HTML5 Canvas/main.js
@@ -7,3 +7,33 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(event) {
+    if (!isDrawing) {
+        return;
+    }
+    console.log(event);
+    ctx.beginPath();
+    // Start from
+    ctx.moveTo(lastX, lastY);
+    // Go to
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [event.offsetX, event.offsetY];
+}
+
+canvas.addEventListener('mousedown', function() {
+    isDrawing = true;
+    [lastX, lastY] = [event.offsetX, event.offsetY];
+});
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', function() {
+    isDrawing = false;
+});
+canvas.addEventListener('mouseout', function() {
+    isDrawing = false;
+})
