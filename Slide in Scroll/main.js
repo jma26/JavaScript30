35cb44ea8a49da75e60b6bf18d6ce44368ab1d2e
Jesse Ma <j.masonoma17@gmail.com>
2018-06-25 23:41:27 -0700

    Add if/else statement to add classList

--- Slide in Scroll/main.js
+++ Slide in Scroll/main.js
@@ -23,6 +23,12 @@ function checkSlide(event) {
         const imageBottom = sliderImage.offsetTop + sliderImage.height;
         const isHalfShown = slideInAt > sliderImage.offsetTop;
         const isNotScrolledPast = window.scrollY < imageBottom;
+
+        if (isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        }
     })
 }
 
