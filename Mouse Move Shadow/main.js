9c1b4fd8f287a7f9961501c4501cf588322d4571
Jesse Ma <j.masonoma17@gmail.com>
2018-07-06 01:59:53 -0700

    Update consts-xWalk, yWalk and add textShadow values to const-text

--- Mouse Move Shadow/main.js
+++ Mouse Move Shadow/main.js
@@ -14,9 +14,11 @@ function shadow(event) {
         y = y + event.target.offsetTop;
     }
 
-    const xWalk = Math.round((x/ x * width) - (walk / 2));
-    const yWalk = Math.round((y/ y * height) - (walk / 2));
     
+    const xWalk = Math.round((x/ width * walk) - (walk / 2));
+    const yWalk = Math.round((y/ height * walk) - (walk / 2));
+    
+    text.style.textShadow = `${xWalk}px ${yWalk}px 0 red`;
 }
 
 hero.addEventListener('mousemove', shadow);
