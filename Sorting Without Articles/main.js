d54a662d51ea5fc6b4c28d589b3ec7554284143f
Jesse Ma <j.masonoma17@gmail.com>
2018-07-07 23:23:39 -0700

    Add stripArticles() to find and replace article words

--- Sorting Without Articles/main.js
+++ Sorting Without Articles/main.js
@@ -1,7 +1,11 @@
 const players = ['The Kento Momota', 'The Lin Dan', 'Shi YuQi', 'Lee Chong Wei', 'Srikanth Kidambi', 'Won Shon Ho', 'Chen Long', 'The Viktor Axelsen', 'An Anthony Ginting', 'Jonatan Christie', ' A Brice Leverdez', 'Kanta Tsuneyama', 'Kenta Nishimoto', 'Tien Minh Nguyen'];
 
+function stripArticles(player) {
+    return player.replace(/^(a |the |an )/i, '');
+}
+
 const sortedPlayers = players.sort(function(a, b) {
-    if (a > b) {
+    if (stripArticles(a) > stripArticles(b)) {
         return 1;
     } else {
         return -1;
