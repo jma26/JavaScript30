f82d10fd89a2e3cd3e0bdc5519262f7cd55f7e30
Jesse Ma <j.masonoma17@gmail.com>
2018-07-07 23:18:19 -0700

    Sort the array of players alphabetically

--- Sorting Without Articles/main.js
+++ Sorting Without Articles/main.js
@@ -1,1 +1,9 @@
 const players = ['The Kento Momota', 'The Lin Dan', 'Shi YuQi', 'Lee Chong Wei', 'Srikanth Kidambi', 'Won Shon Ho', 'Chen Long', 'The Viktor Axelsen', 'An Anthony Ginting', 'Jonatan Christie', ' A Brice Leverdez', 'Kanta Tsuneyama', 'Kenta Nishimoto', 'Tien Minh Nguyen'];
+
+const sortedPlayers = players.sort(function(a, b) {
+    if (a > b) {
+        return 1;
+    } else {
+        return -1;
+    }
+});
