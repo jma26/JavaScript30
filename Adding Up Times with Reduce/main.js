ca4b1147b880bb43ed366048a59764bc0bb8ef21
Jesse Ma <j.masonoma17@gmail.com>
2018-07-08 19:32:54 -0700

    Retrieve hours, minutes, and seconds left

--- Adding Up Times with Reduce/main.js
+++ Adding Up Times with Reduce/main.js
@@ -4,7 +4,21 @@ const seconds = timeNodes.map(function(node) {
     return node.dataset.time
 }).map(function(timeCode) {
     const [mins, secs] = timeCode.split(':').map(parseFloat);
-    console.log(mins, secs);
     return (mins * 60) + secs;
-})
+}).reduce(function(total, seconds) {
+    return total + seconds
+});
+
+let secondsLeft = seconds;
+// Get hours
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+// Get minutes
+const mins = Math.floor(secondsLeft / 60);
+// Get seconds
+secondsLeft = secondsLeft % 60;
+
+console.log(hours, mins, secondsLeft);
+
+
 
